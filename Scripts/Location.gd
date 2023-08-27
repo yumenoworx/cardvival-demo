f6cf0349f912ffeef55ee8e213a035c8a8a1f6cd
natsvnxkaze <natsvnxkaze@proton.me>
2023-08-28 01:45:57 +0300

    Fix my spawner slots centering pls (Main.gd)
    ヾ(•ω•`)o oh hello!

--- Scripts/Location.gd
+++ Scripts/Location.gd
@@ -1,4 +1,62 @@
-extends Node2D
+extends KinematicBody2D
+
+
+var grab_x = 0
+var grab_y = 0
+
+var id = "forest"
+var cooldown = false
+var cooldown_time = 3
+var can_move = false
+var dragging = false
+
+var old_x = 0
+var old_y = 0
+
+var put_down = false
 
 func _process(delta):
-	return
+	if can_move:
+		position.x = get_viewport().get_mouse_position().x - grab_x
+		position.y = get_viewport().get_mouse_position().y - grab_y
+		position.x = clamp(position.x, 0, 1366)
+		position.y = clamp(position.y, 0, 768)
+		if position.x != old_x or position.y != old_y and dragging == false:
+			dragging = true
+	if cooldown:
+		cooldown_time -= delta
+		if cooldown_time <= 0:
+			cooldown = false
+			cooldown_time = 3
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("left_mb"):
+			put_down = false
+			can_move = true
+			$Sprite.scale.x = 0.32 + 0.017
+			$Sprite.scale.y = 0.32 + 0.017
+			grab_x = get_viewport().get_mouse_position().x - position.x
+			grab_y = get_viewport().get_mouse_position().y - position.y
+			old_x = position.x
+			old_y = position.y
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		if event.is_action_released("left_mb"):
+			put_down = true
+			can_move = false
+			dragging = false
+			$Sprite.scale.x = 0.32
+			$Sprite.scale.y = 0.32
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_Area2D_mouse_entered():
+	if not cooldown:
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_WAIT)
+
+
+func _on_Area2D_mouse_exited():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
---
+++ Scripts/Main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+var slots = 3
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var i = 0
+	while i < slots:
+		i += 1

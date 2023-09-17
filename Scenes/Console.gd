c3641f694f2332cdf437d7443ebe41ddcf6bc076
natsu/ <141510473+natsvnxkaze@users.noreply.github.com>
2023-09-17 11:52:35 +0300

    Improved code quality for console

--- Scenes/Console.gd
+++ Scenes/Console.gd
@@ -26,35 +26,39 @@ func _on_text_edit_text_changed():
 func _on_button_pressed():
 	if command == "":
 		send("Nothing happened...")
-	elif command.split(" ")[0] == "quit":
-		get_tree().quit()
-	elif command.split(" ")[0] == "echo":
-		var text = command.split(" ", true, 1)[1]
-		send(text)
-	elif command.split(" ")[0] == "help":
-		send("Nobody help you.")
-	elif command.split(" ")[0] == "spawn_instance":
-		var instance = null
-		match command.split(" ")[1]:
-			"tool": instance = "res://Scenes/Cards/Tool.tscn"
-			"location": instance = "res://Scenes/Cards/Location.tscn"
-			"resource": instance = "res://Scenes/Cards/Resource.tscn"
-			_: $Output.text = "spawn_instance [tool|location|resource]"; return
-		var item = load(instance).instantiate()
-		get_tree().get_root().get_node("Main").get_node("Cards").add_child(item)
-		item.position = get_viewport().get_mouse_position()
-	elif command.split(" ")[0] == "remove_instance":
-		var tags = []
-		for node in get_tree().get_root().get_node("Main").get_node("Cards").get_children(false):
-			var pos = node.position - get_viewport().get_mouse_position()
-			print(pos)
-			if pos.x >= -59 and pos.x <= 59 and pos.y >= -82 and pos.y <= 82:
-				tags.append(node.tag)
-				node.queue_free()
-		send("Deleted: " + str(tags))
-		print($Output.text)
-	else:
-		send(command + " command doesn't exist.")
+	match command.split(" ")[0]:
+		"quit":
+			get_tree().quit()
+		"echo":
+			var text = command.split(" ", true, 1)[1]
+			send(text)
+		"help":
+			send("Nobody help you.")
+		"spawn_instance":
+			var instance = null
+			match command.split(" ")[1]:
+				"tool": instance = "res://Scenes/Cards/Tool.tscn"
+				"location": instance = "res://Scenes/Cards/Location.tscn"
+				"resource": instance = "res://Scenes/Cards/Resource.tscn"
+				_: $Output.text = "spawn_instance [tool|location|resource]"; return
+			var item = load(instance).instantiate()
+			get_tree().get_root().get_node("Main").get_node("Cards").add_child(item)
+			item.position = get_viewport().get_mouse_position()
+			send("Spawned: " + command.split(" ")[1])
+		"remove_instance":
+			var tags = []
+			for node in get_tree().get_root().get_node("Main").get_node("Cards").get_children(false):
+				var pos = node.position - get_viewport().get_mouse_position()
+				print(pos)
+				if pos.x >= -59 and pos.x <= 59 and pos.y >= -82 and pos.y <= 82:
+					tags.append(node.tag)
+					node.queue_free()
+			send("Deleted: " + str(tags))
+			print($Output.text)
+		_:
+			send(command + " command doesn't exist.")
+
+
 func send(info: String):
 	$Output.text = $Output.text + "\n" + $TextEdit.text + "\n>> " + info
 	$TextEdit.clear()
